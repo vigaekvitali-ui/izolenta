8475fbf5a43721c260fb9e2a90e3bc7f3581283f
vigaekvitali-ui <vigaekvitali@gmail.com>
2026-02-20 17:19:42 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'artel-v1';
+const assets = [
+    '/',
+    '/index.html',
+    'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
